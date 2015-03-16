eb918d97df750be072dbdfa113ff8da97a6acb0d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-03-16 12:22:43 +0000

    End of e8

---
+++ src/test/java/shouty/ShoutSupport.java
@@ -0,0 +1,24 @@
+package shouty;
+
+import cucumber.api.java.Before;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ShoutSupport {
+    public final Map<String, Person> people = new HashMap<String, Person>();
+    public final Map<String, List<String>> messagesShoutedBy = new HashMap<String, List<String>>();
+
+    public void seanShout(String message) {
+        people.get("Sean").shout(message);
+        List<String> messages = messagesShoutedBy.get("Sean");
+        if (messages == null) {
+            messages = new ArrayList<String>();
+            messagesShoutedBy.put("Sean", messages);
+        }
+        messages.add(message);
+    }
+
+}
--- src/test/java/shouty/Stepdefs.java
+++ src/test/java/shouty/Stepdefs.java
@@ -1,7 +1,6 @@
 package shouty;
 
 import cucumber.api.DataTable;
-import cucumber.api.PendingException;
 import cucumber.api.Transpose;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
@@ -21,14 +20,11 @@ import static org.junit.Assert.assertThat;
 
 public class Stepdefs {
 
+    private final ShoutSupport shoutSupport;
     private Network network;
-    private Map<String, Person> people;
-    private Map<String, List<String>> messagesShoutedBy;
 
-    @Before
-    public void setup() {
-        people = new HashMap<String, Person>();
-        messagesShoutedBy = new HashMap<String, List<String>>();
+    public Stepdefs(ShoutSupport shoutSupport) {
+        this.shoutSupport = shoutSupport;
     }
 
     @Given("^the range is (\\d+)$")
@@ -43,45 +39,45 @@ public class Stepdefs {
 
     @Given("^Sean has bought (\\d+) credits$")
     public void sean_has_bought_credits(int credits) throws Throwable {
-        people.get("Sean").setCredits(credits);
+        shoutSupport.people.get("Sean").setCredits(credits);
     }
 
     @Given("^the following people:$")
     public void the_following_people(@Transpose List<Whereabouts> whereabouts) throws Throwable {
         for (Whereabouts whereabout : whereabouts) {
-            people.put(whereabout.name, new Person(network, whereabout.location));
+            shoutSupport.people.put(whereabout.name, new Person(network, whereabout.location));
         }
     }
 
     @When("^Sean shouts a message containing the word \"(.*?)\"$")
     public void sean_shouts_a_message_containing_the_word(String word) throws Throwable {
-        shout("a message containing the word " + word);
+        shoutSupport.seanShout("a message containing the word " + word);
     }
 
     @When("^Sean shouts (\\d+) messages containing the word \"(.*?)\"$")
     public void sean_shouts_messages_containing_the_word(int num, String word) throws Throwable {
         for(int j = 0; j < num; j++) {
-            shout("a message containing the word " + word);
+            shoutSupport.seanShout("a message containing the word " + word);
         }
     }
 
     @When("^Sean shouts a message$")
     public void sean_shouts_a_message() throws Throwable {
-        shout("here is a message");
+        shoutSupport.seanShout("here is a message");
     }
 
     @When("^Sean shouts a long message$")
     public void sean_shouts_a_long_message() throws Throwable {
         String longMessage = "";
         for(int i = 0; i < 180; i++) longMessage += "x";
-        shout(longMessage);
+        shoutSupport.seanShout(longMessage);
     }
 
     @When("^Sean shouts an over-long message$")
     public void sean_shouts_an_over_long_message() throws Throwable {
         String longMessage = "";
         for(int i = 0; i < 181; i++) longMessage += "x";
-        shout(longMessage);
+        shoutSupport.seanShout(longMessage);
     }
 
     @When("^Sean shouts (\\d+) over-long message$")
@@ -89,28 +85,18 @@ public class Stepdefs {
         for(int j = 0; j < num; j++) {
             String longMessage = "";
             for(int i = 0; i < 181; i++) longMessage += "x";
-            shout(longMessage);
+            shoutSupport.seanShout(longMessage);
         }
     }
 
     @When("^Sean shouts \"(.*?)\"$")
     public void sean_shouts(String message) throws Throwable {
-        shout(message);
+        shoutSupport.seanShout(message);
     }
 
     @When("^Sean shouts:$")
     public void sean_shouts_longer_message(String message) throws Throwable {
-        shout(message);
-    }
-
-    private void shout(String message) {
-        people.get("Sean").shout(message);
-        List<String> messages = messagesShoutedBy.get("Sean");
-        if (messages == null) {
-            messages = new ArrayList<String>();
-            messagesShoutedBy.put("Sean", messages);
-        }
-        messages.add(message);
+        shoutSupport.seanShout(message);
     }
 
     @Then("^Lucy hears Sean's message$")
@@ -120,8 +106,8 @@ public class Stepdefs {
 
     @Then("^Lucy hears all Sean's messages$")
     public void lucy_hears_all_Sean_s_messages() throws Throwable {
-        List<String> heardByLucy = people.get("Lucy").getMessagesHeard();
-        List<String> messagesFromSean = messagesShoutedBy.get("Sean");
+        List<String> heardByLucy = shoutSupport.people.get("Lucy").getMessagesHeard();
+        List<String> messagesFromSean = shoutSupport.messagesShoutedBy.get("Sean");
 
         // Hamcrest's hasItems matcher wants an Array, not a List.
         String[] messagesFromSeanArray = messagesFromSean.toArray(new String[messagesFromSean.size()]);
@@ -131,7 +117,7 @@ public class Stepdefs {
     @Then("^Lucy hears the following messages:$")
     public void lucy_hears_the_following_messages(DataTable expectedMessages) throws Throwable {
         List<List<String>> actualMessages = new ArrayList<List<String>>();
-        List<String> heard = people.get("Lucy").getMessagesHeard();
+        List<String> heard = shoutSupport.people.get("Lucy").getMessagesHeard();
         for (String message : heard) {
             actualMessages.add(asList(message));
         }
@@ -140,23 +126,23 @@ public class Stepdefs {
 
     @Then("^Larry does not hear Sean's message$")
     public void larry_does_not_hear_Sean_s_message() throws Throwable {
-        List<String> heardByLarry = people.get("Larry").getMessagesHeard();
-        List<String> messagesFromSean = messagesShoutedBy.get("Sean");
+        List<String> heardByLarry = shoutSupport.people.get("Larry").getMessagesHeard();
+        List<String> messagesFromSean = shoutSupport.messagesShoutedBy.get("Sean");
         String[] messagesFromSeanArray = messagesFromSean.toArray(new String[messagesFromSean.size()]);
         assertThat(heardByLarry, not(hasItems(messagesFromSeanArray)));
     }
 
     @Then("^nobody hears Sean's message$")
     public void nobody_hears_Sean_s_message() throws Throwable {
-        List<String> messagesFromSean = messagesShoutedBy.get("Sean");
+        List<String> messagesFromSean = shoutSupport.messagesShoutedBy.get("Sean");
         String[] messagesFromSeanArray = messagesFromSean.toArray(new String[messagesFromSean.size()]);
-        for (Person person : people.values()) {
+        for (Person person : shoutSupport.people.values()) {
             assertThat(person.getMessagesHeard(), not(hasItems(messagesFromSeanArray)));
         }
     }
 
     @Then("^Sean should have (\\d+) credits$")
     public void sean_should_have_credits(int credits) throws Throwable {
-        assertEquals(credits, people.get("Sean").getCredits());
+        assertEquals(credits, shoutSupport.people.get("Sean").getCredits());
     }
 }
